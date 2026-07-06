51a47b3f4742278da22c8013bae032b2713a9250
milanm5 <milanmiljkovic10012@gmail.com>
2026-07-06 08:56:47 +0200

    day 10 complete

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
+
+    use HasFactory;
+
 
     // klasa treba da se zovi isto kao i naziv tabele u bazi samo u jednini, ako to nije slucaj mozemo postaviti vrednost $table sa nazivom nase tabele u bazi
     protected $table = 'job_listings';
