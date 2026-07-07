ef977699282a83a2d2d2bba8d0ea0513b997e89c
milanm5 <milanmiljkovic10012@gmail.com>
2026-07-07 14:51:45 +0200

    day 17 complete

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,10 @@ Route::get('/jobs/{id}', function ($id) {
 
 Route::post('/jobs', function () {
     //validations
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
 
     Job::create([
         'title' => request('title'),
