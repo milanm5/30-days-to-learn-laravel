0c6f57968a9d1365ae62533b788bf73c236b2be4
milanm5 <milanmiljkovic10012@gmail.com>
2026-07-07 14:33:37 +0200

    day 16 complete

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,12 +9,13 @@ class Job extends Model {
 
     use HasFactory;
 
-
     // klasa treba da se zovi isto kao i naziv tabele u bazi samo u jednini, ako to nije slucaj mozemo postaviti vrednost $table sa nazivom nase tabele u bazi
     protected $table = 'job_listings';
 
     // ovde navodimo kolone koje mogu biti popunjene od strane korisnika
-    protected $fillable = ['title', 'salary'];
+    // protected $fillable = ['title', 'salary', 'employer_id'];
+
+    protected $guarded = []; // kontra od ovog ^
 
     public function employer() {
         return $this->belongsTo(Employer::class);
