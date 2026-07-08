d98a24f46eeec16c1b2390415b9877ba64ef9ddc
milanm5 <milanmiljkovic10012@gmail.com>
2026-07-08 09:41:37 +0200

    day 20 complete

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -16,10 +17,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id, ['*']);
 
@@ -28,6 +31,7 @@ Route::get('/jobs/{id}', function ($id) {
     ]);
 });
 
+// Store
 Route::post('/jobs', function () {
     //validations
     request()->validate([
@@ -42,6 +46,43 @@ Route::post('/jobs', function () {
     ]);
 
     return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id, ['*']);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+     'title'=> ['required','min:3'],
+     'salary' => ['required']
+    ]);
+    // authorize (on hold)
+
+    // update the job
+    $job = Job::findOrFail($id, ['*']);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);;
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+
+    // authorize
+    // delete the job
+    Job::findOrFail($id, ['*'])->delete();
+
+    return redirect("/jobs");
 });
 
 Route::get('/contact', function () {
