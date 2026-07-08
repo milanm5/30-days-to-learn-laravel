935769e775b6d29e54d84df5ec66363754f385ec
milanm5 <milanmiljkovic10012@gmail.com>
2026-07-08 10:46:53 +0200

    day 19 complete

--- routes/web.php
+++ routes/web.php
@@ -1,90 +1,10 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-    return view('home');
-});
+Route::resource('jobs', JobController::class);
 
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-// Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Show
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id, ['*']);
-
-    return view('jobs.show', [
-        'job' => $job
-    ]);
-});
-
-// Store
-Route::post('/jobs', function () {
-    //validations
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id, ['*']);
-
-    return view('jobs.edit', ['job' => $job]);
-});
-
-// Update
-Route::patch('/jobs/{id}', function ($id) {
-    // validate
-    request()->validate([
-     'title'=> ['required','min:3'],
-     'salary' => ['required']
-    ]);
-    // authorize (on hold)
-
-    // update the job
-    $job = Job::findOrFail($id, ['*']);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);;
-
-    return redirect('/jobs/' . $job->id);
-});
-
-// Destroy
-Route::delete('/jobs/{id}', function ($id) {
-
-    // authorize
-    // delete the job
-    Job::findOrFail($id, ['*'])->delete();
-
-    return redirect("/jobs");
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
